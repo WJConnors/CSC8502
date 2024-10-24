9d34b9b2283e8532681f3128f8267b13c8bc9c0a
WJConnors <billy.connors@yahoo.co.uk>
2024-10-24 15:21:30 +0100

    Tutorials 3 and 4

--- 4) Depth & Transparency/tutorial4.cpp
+++ 4) Depth & Transparency/tutorial4.cpp
@@ -32,7 +32,6 @@ int main()	{
 		if(Window::GetKeyboard()->KeyDown(KEYBOARD_DOWN)) {
 			renderer.MoveObject(-0.1f);
 		}
-
 		renderer.RenderScene();
 		renderer.SwapBuffers();
 		if (Window::GetKeyboard()->KeyDown(KEYBOARD_F5)) {
